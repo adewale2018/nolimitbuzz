d3feb3952b85bc9fe6f55a9c3d45f9689aa298c0
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2025-01-03 07:34:32 +0100

    Implement pagination

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow p-5">
+    <nav className="bg-white shadow p-5 flex items-center justify-between flex-wrap">
       <Link to="/">
         <img
           style={{ width: "200px" }}
@@ -11,6 +11,9 @@ export default function Navbar() {
           alt="nolimitbuzz icon"
         />
       </Link>
+      <a href="https://github.com/adewale2018/nolimitbuzz" target="_blank" className="cursor-pointer text-sky-600 hover:text-sky-700">
+        Source code
+      </a>
     </nav>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,7 +4,15 @@ import UserData from "../components/UserData";
 import { useEffect } from "react";
 import { useStore } from "../store";
 const Homepage = () => {
-  const { loading, users, getUsers } = useStore();
+  const {
+    loading,
+    users,
+    getUsers,
+    currentPage,
+    totalUsers,
+    setPage,
+    pageSize,
+  } = useStore();
 
   useEffect(() => {
     getUsers();
@@ -20,6 +28,22 @@ const Homepage = () => {
         {users?.map((user) => (
           <UserData key={user?.id} {...user} />
         ))}
+      </div>
+      <div className="flex items-center justify-end mt-16">
+        <button
+          onClick={() => setPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="bg-sky-500 text-white px-5 py-2 rounded-md mr-2 disabled:bg-gray-200 disabled:text-gray-500"
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setPage(currentPage + 1)}
+          disabled={currentPage === Math.ceil(totalUsers / pageSize)}
+          className="bg-white text:bg-sky-500 hover:bg-sky-500 border border-sky-500 hover:text-white px-5 py-2 rounded-md mr-2 disabled:bg-gray-200 disabled:text-gray-500 transition-all duration-100"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
